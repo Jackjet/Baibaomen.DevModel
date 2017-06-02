079c81eef917701051735674f8168b3d7da436c8
baibaomen <baibaomen@gmail.com>
2017-06-03 00:34:51 +0800

    添加ApiWeb，引用包已更新至最新。 注意：IdentityModel只能引用2.0以下版本。

---
+++ Baibaomen.DevModel.ApiWeb/Controllers/PropertyController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using Baibaomen.DevModel.ApiWeb.Models;
+using Baibaomen.DevModel.Businsess.DomainServices;
+using Baibaomen.DevModel.Businsess.Entities;
+using Baibaomen.DevModel.Infrastructure;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.OData.Query;
+
+namespace Baibaomen.DevModel.ApiWeb.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [RoutePrefix("api/property")]
+    public class PropertyController : ApiController
+    {
+        PropertyService _propertyService;
+        UserService _userService;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyService"></param>
+        /// <param name="userService"></param>
+        public PropertyController(PropertyService propertyService,UserService userService) {
+            _propertyService = propertyService;
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [Route("")]
+        [ResponseType(typeof(PropertyViewModel))]
+        public async Task<IHttpActionResult> Post(PropertyAndCommunicationCreateModel model) {
+            var toReturn = await _propertyService.AddPropertyAsync(model, this.GetOperator(_userService));
+            return CreatedAtRoute("property", new { id = toReturn.Id }, toReturn);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [Route("{id}")]
+        [ResponseType(typeof(PropertyViewModel))]
+        public async Task<IHttpActionResult> Put(int id,PropertyUpdateModel model) {
+            return Ok(await _propertyService.UpdatePropertyAsync(id,model,this.GetOperator(_userService)));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        [Auth()]
+        [Route("")]
+        [ResponseType(typeof(PagedResult<PropertyViewModel>))]
+        public async Task<IHttpActionResult> GetAll(ODataQueryOptions<Property> options) {
+            var result = _propertyService.GetAllProperties(this.GetOperator(_userService));
+            return Ok(options.FilterResult<PropertyViewModel, Property>(result, this));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        [Route("{id}", Name = "property")]
+        [ResponseType(typeof(PropertyViewModel))]
+        public async Task<IHttpActionResult> Get(int id, ODataQueryOptions<PropertyViewModel> options)
+        {
+            var theProperty = _propertyService.GetProperty(id, this.GetOperator(_userService));
+
+            if (theProperty == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(options.FilterSingleResult(theProperty, this));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="recordVersion"></param>
+        /// <returns></returns>
+        [Route("{id}")]
+        public async Task<IHttpActionResult> Delete(int id, string recordVersion)
+        {
+            await _propertyService.DeleteAsync(id, Convert.FromBase64String(recordVersion), this.GetOperator(_userService));
+            return Ok();
+        }
+    }
+}
---
+++ Baibaomen.DevModel.ApiWeb/Startup.cs
@@ -0,0 +1,154 @@
+﻿using Autofac;
+using Autofac.Features.ResolveAnything;
+using Autofac.Integration.WebApi;
+using AutoMapper;
+using Baibaomen.DevModel.ApiWeb;
+using Baibaomen.DevModel.Business.AutoMapper;
+using Baibaomen.DevModel.Infrastructure;
+using IdentityServer3.AccessTokenValidation;
+using log4net;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Swashbuckle.Application;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+
+/// <summary>
+/// 
+/// </summary>
+public class Startup
+{
+    string authority = ConfigurationManager.AppSettings["CA.Address"];
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="app"></param>
+    public void Configuration(IAppBuilder app)
+    {
+        HttpConfiguration config = new HttpConfiguration();
+
+        ConfigureExceptionAndLog(app, config);
+
+        ConfigureSwagger(config);
+
+        ConfigureJson(app, config);
+
+        ConfigureWebApi(app, config);
+
+        ConfigureAutofac(app, config);
+
+        ConfigureAutoMapper();
+
+        ConfigureIdentityServer(app);
+
+        app.UseWebApi(config);
+    }
+
+    private void ConfigureIdentityServer(IAppBuilder app)
+    {
+        System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler.Defa‌​ultInboundClaimTypeM‌​ap = new Dictionary<string, string>();
+
+        app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+        {
+            Authority = authority,
+            RequiredScopes = "api".Split(' ')
+        });
+
+        //// add app local claims per request
+        //app.UseClaimsTransformation(incoming =>
+        //{
+        //    // either add claims to incoming, or create new principal
+        //    var appPrincipal = new ClaimsPrincipal(incoming);
+        //    incoming.Identities.First().AddClaim(new Claim("appSpecific", "some_value"));
+
+        //    return Task.FromResult(appPrincipal);
+        //});
+    }
+
+    private void ConfigureAutofac(IAppBuilder app, HttpConfiguration config)
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+        builder.RegisterModule(new AutofacModule());
+        builder.RegisterModule(new Baibaomen.DevModel.Businsess.AutofacModule());
+
+        var container = builder.Build();
+        config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        app.UseAutofacMiddleware(container);
+        app.UseAutofacWebApi(config);
+    }
+
+    private void ConfigureAutoMapper()
+    {
+        Mapper.Initialize(cfg =>
+        {
+            cfg.AddProfile<ViewModelsProfile>();
+        });
+    }
+
+    private void ConfigureJson(IAppBuilder app, HttpConfiguration config)
+    {
+        var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+        jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        jsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+        jsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+    }
+
+    private void ConfigureSwagger(HttpConfiguration config)
+    {
+        config.EnableSwagger(c =>
+        {
+            c.SingleApiVersion("v1", "WebAPI");
+            c.IncludeXmlComments(GetXmlCommentsPath());
+
+            c.OAuth2("oauth2")
+              .Description("api")
+              .Flow("implicit")
+              .AuthorizationUrl(authority + "connect/authorize")
+              .Scopes(scopes =>
+              {
+                  scopes.Add("api", "");
+              });
+
+            //Apply oauth filter.
+            c.OperationFilter<OAuth2OperationFilter>();
+
+        }).EnableSwaggerUi(c =>
+            c.EnableOAuth2Support("api_doc", "238B3DFC-5AC1-42D7-A705-2B6A3356EFC9", "test_realm", "Baibaomen Api")
+        );
+    }
+
+    private static string GetXmlCommentsPath()
+    {
+        return String.Format(@"{0}\bin\Baibaomen.DevModel.ApiWeb.XML",
+                AppDomain.CurrentDomain.BaseDirectory);
+    }
+
+    private void ConfigureWebApi(IAppBuilder app, HttpConfiguration config)
+    {
+        config.MapHttpAttributeRoutes();
+
+        config.AddModelValidationFilter();
+
+        app.UseCors(CorsOptions.AllowAll);
+
+        config.EnableETag(new List<string>() { "/api/" }, ExecutionMode.Include);
+    }
+
+    private void ConfigureExceptionAndLog(IAppBuilder app, HttpConfiguration config)
+    {
+        ILog logger = LogManager.GetLogger("logger");
+
+        config.Services.Replace(typeof(IExceptionHandler), new UnhandledExceptionHandler(x => logger.Error($"Unhandled exception occurred\nURL:\n{x.Request.RequestUri}\nHeaders:\n{x.Request.Headers}", x.Exception)));
+    }
+}
