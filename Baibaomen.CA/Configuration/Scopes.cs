4f1aa793eb8633be2cac4b2026bd9294a9c720c8
baibaomen <baibaomen@gmail.com>
2017-06-02 23:04:24 +0800

    更新所有nuget包到最新版本。可用版。 Baibaomen.CA的Clients、Scopes、Startup清理掉不需要的设置。

--- Baibaomen.CA/Configuration/Scopes.cs
+++ Baibaomen.CA/Configuration/Scopes.cs
@@ -44,41 +44,11 @@ namespace IdentityServer3.Host.Config
 
                     new Scope
                     {
-                        Name = "read",
-                        DisplayName = "Read data",
-                        Type = ScopeType.Resource,
-                        Emphasize = false,
-
-                        ScopeSecrets = new List<Secret>
-                        {
-                            new Secret("secret".Sha256())
-                        }
-                    },
-                    new Scope
-                    {
-                        Name = "write",
-                        DisplayName = "Write data",
+                        Name = "api",
+                        DisplayName = "API access",
                         Type = ScopeType.Resource,
                         Emphasize = true,
-
-                        ScopeSecrets = new List<Secret>
-                        {
-                            new Secret("secret".Sha256())
-                        }
-                    },
-                    new Scope
-                    {
-                        Name = "idmgr",
-                        DisplayName = "IdentityManager",
-                        Type = ScopeType.Resource,
-                        Emphasize = true,
-                        ShowInDiscoveryDocument = false,
-                        
-                        Claims = new List<ScopeClaim>
-                        {
-                            new ScopeClaim(Constants.ClaimTypes.Name),
-                            new ScopeClaim(Constants.ClaimTypes.Role)
-                        }
+                        IncludeAllClaimsForUser = true
                     }
                 };
         }
--- Baibaomen.CA/Startup.cs
+++ Baibaomen.CA/Startup.cs
@@ -32,11 +32,11 @@ namespace WebHost
     {
         public void Configuration(IAppBuilder app)
         {
-            LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider());
-            Log.Logger = new LoggerConfiguration()
-               .MinimumLevel.Debug()
-               .WriteTo.Trace()
-               .CreateLogger();
+            //LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider());
+            //Log.Logger = new LoggerConfiguration()
+            //   .MinimumLevel.Debug()
+            //   .WriteTo.Logger(log4net.LogManager.GetLogger()).Trace()
+            //   .CreateLogger();
 
             app.Map("/admin", adminApp =>
             {
@@ -58,47 +58,48 @@ namespace WebHost
 
                 var options = new IdentityServerOptions
                 {
-                    SiteName = "IdentityServer3 - UserService-AspNetIdentity",
+                    //SiteName = "IdentityServer3 - UserService-AspNetIdentity",
+                    SiteName = "Baibaomen.CA",
                     SigningCertificate = Certificate.Get(),
                     Factory = idSvrFactory,
-                    AuthenticationOptions = new AuthenticationOptions
-                    {
-                        IdentityProviders = ConfigureAdditionalIdentityProviders,
-                    }
+                    //AuthenticationOptions = new AuthenticationOptions
+                    //{
+                    //    IdentityProviders = ConfigureAdditionalIdentityProviders,
+                    //}
                 };
 
                 core.UseIdentityServer(options);
             });
         }
 
-        public static void ConfigureAdditionalIdentityProviders(IAppBuilder app, string signInAsType)
-        {
-            var google = new GoogleOAuth2AuthenticationOptions
-            {
-                AuthenticationType = "Google",
-                SignInAsAuthenticationType = signInAsType,
-                ClientId = "767400843187-8boio83mb57ruogr9af9ut09fkg56b27.apps.googleusercontent.com",
-                ClientSecret = "5fWcBT0udKY7_b6E3gEiJlze"
-            };
-            app.UseGoogleAuthentication(google);
+        //public static void ConfigureAdditionalIdentityProviders(IAppBuilder app, string signInAsType)
+        //{
+        //    var google = new GoogleOAuth2AuthenticationOptions
+        //    {
+        //        AuthenticationType = "Google",
+        //        SignInAsAuthenticationType = signInAsType,
+        //        ClientId = "767400843187-8boio83mb57ruogr9af9ut09fkg56b27.apps.googleusercontent.com",
+        //        ClientSecret = "5fWcBT0udKY7_b6E3gEiJlze"
+        //    };
+        //    app.UseGoogleAuthentication(google);
 
-            var fb = new FacebookAuthenticationOptions
-            {
-                AuthenticationType = "Facebook",
-                SignInAsAuthenticationType = signInAsType,
-                AppId = "676607329068058",
-                AppSecret = "9d6ab75f921942e61fb43a9b1fc25c63"
-            };
-            app.UseFacebookAuthentication(fb);
+        //    var fb = new FacebookAuthenticationOptions
+        //    {
+        //        AuthenticationType = "Facebook",
+        //        SignInAsAuthenticationType = signInAsType,
+        //        AppId = "676607329068058",
+        //        AppSecret = "9d6ab75f921942e61fb43a9b1fc25c63"
+        //    };
+        //    app.UseFacebookAuthentication(fb);
 
-            var twitter = new TwitterAuthenticationOptions
-            {
-                AuthenticationType = "Twitter",
-                SignInAsAuthenticationType = signInAsType,
-                ConsumerKey = "N8r8w7PIepwtZZwtH066kMlmq",
-                ConsumerSecret = "df15L2x6kNI50E4PYcHS0ImBQlcGIt6huET8gQN41VFpUCwNjM"
-            };
-            app.UseTwitterAuthentication(twitter);
-        }
+        //    var twitter = new TwitterAuthenticationOptions
+        //    {
+        //        AuthenticationType = "Twitter",
+        //        SignInAsAuthenticationType = signInAsType,
+        //        ConsumerKey = "N8r8w7PIepwtZZwtH066kMlmq",
+        //        ConsumerSecret = "df15L2x6kNI50E4PYcHS0ImBQlcGIt6huET8gQN41VFpUCwNjM"
+        //    };
+        //    app.UseTwitterAuthentication(twitter);
+        //}
     }
 }
